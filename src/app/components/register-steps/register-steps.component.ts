c884d9f083e5d6f8de3bba842cc938c12ad856ce
Debby ermel <debbyermel@gmail.com>
2018-08-19 20:31:03 -0500

    Finished register steps views

---
+++ src/app/components/register-steps/register-steps.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-steps',
+  templateUrl: './register-steps.component.html',
+  styleUrls: ['./register-steps.component.css']
+})
+export class RegisterStepsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
